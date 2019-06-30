d24b54911f4f974aca93448b273ec1b0dbba766a
Christopher Hustman <33296214+christopher-hustman@users.noreply.github.com>
2019-06-30 19:27:12 -0400

    basic tableview working

---
+++ PracticalCodingTest/UserTableViewController.cs
@@ -0,0 +1,18 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace PracticalCodingTest
+{
+    public partial class UserTableViewController : UITableViewController
+    {
+        public UserTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+        }
+    }
+}
